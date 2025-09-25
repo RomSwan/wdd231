7d031c927e23868fc81c4bd909669708a0ac3f38
RomSwan <legion.of.swans@gmail.com>
2025-09-25 09:41:54 +0200

    completed modals

---
+++ chamber/scripts/membership-level.js
@@ -0,0 +1,45 @@
+// Fetch Data
+const json = "data/levels.json";
+
+async function getLevelInfo(json) {
+    const response = await fetch(json);
+    const data = await response.json();
+    let levels = data.levels;
+
+    const index = {};
+    levels.forEach(level => {
+        index[level.title.toLowerCase()] = level;
+    });
+
+    displayLevelInfo(index);
+}
+
+// Choose and generate modal
+function displayLevelInfo(index) {
+    const dialog = document.getElementById("membership-card");
+
+    document.querySelectorAll(".learn-more").forEach(button => {
+        button.addEventListener("click", () => {
+            const type = button.id
+
+            const level = index[type];
+
+            if (level) {
+                dialog.innerHTML = `
+                    <h2>${level.title} Membership</h2>
+                    <p><b>Price: </b>${level.price}</p>
+                    <p><b>Benefits include: </b>${level.benefits}</p>
+                    <button id="close-dialog">Close</button>
+                `;
+                dialog.showModal();
+
+                dialog.querySelector("#close-dialog").addEventListener("click", () => {
+                    dialog.close();
+                });
+            }
+        });
+    });
+}
+
+// Display
+getLevelInfo(json);
