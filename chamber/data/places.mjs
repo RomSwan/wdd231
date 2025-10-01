424862181d6ec38d1763180fe0b060f4650824c4
RomSwan <legion.of.swans@gmail.com>
2025-10-01 18:00:46 +0200

    mobile cards finished

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -1,58 +1,58 @@
 export const places = [
     {
-        "name": "Royal Castle (Zamek Królewski)",
+        "title": "Royal Castle (Zamek Królewski)",
         "address": "Plac Zamkowy 4, Warsaw, Poland",
         "cost": "Adults: 20 PLN, Students: 10 PLN, Free entry on selected days",
         "description": "Former residence of Polish monarchs in Old Town, richly decorated interiors, royal apartments, museum exhibits and art collections.",
-        "photo_url": "images/royal-castle.webp"
+        "photo": "images/royal-castle.webp"
     },
     {
-        "name": "Palace of Culture and Science (Pałac Kultury i Nauki)",
+        "title": "Palace of Culture and Science (Pałac Kultury i Nauki)",
         "address": "Plac Defilad 1, Warsaw, Poland",
         "cost": "Observation deck: Adults 20 PLN, Students 15 PLN",
         "description": "Iconic Stalin-era skyscraper with theatres, exhibition halls, cinemas, and a 30th-floor observation deck offering panoramic views over Warsaw.",
-        "photo_url": "images/palace-of-culture-and-science.webp"
+        "photo": "images/palace-of-culture-and-science.webp"
     },
     {
-        "name": "POLIN Museum of the History of Polish Jews",
+        "title": "POLIN Museum of the History of Polish Jews",
         "address": "ul. Anielewicza 6, Warsaw, Poland",
         "cost": "Adults: 25 PLN, Students: 15 PLN, Free entry on certain days",
         "description": "Museum tracing 1000 years of Jewish history in Poland, located in what was historically the Warsaw Ghetto. Interactive exhibitions, art, multimedia installations.",
-        "photo_url": "images/polin.webp"
+        "photo": "images/polin.webp"
     },
     {
-        "name": "Wilanów Palace (Muzeum Pałacu Króla Jana III w Wilanowie)",
+        "title": "Wilanów Palace (Muzeum Pałacu Króla Jana III w Wilanowie)",
         "address": "Stanisława Kostki Potockiego 10/16, Warsaw, Poland",
         "cost": "Palace and gardens: Adults 20 PLN, Students 15 PLN, Gardens only: 5 PLN",
         "description": "Baroque royal residence of King Jan III Sobieski surrounded by French-style gardens. Rich interiors, art, and peaceful park grounds.",
-        "photo_url": "images/wilanow-palace.webp"
+        "photo": "images/wilanow-palace.webp"
     },
     {
-        "name": "Copernicus Science Centre (Centrum Nauki Kopernik)",
+        "title": "Copernicus Science Centre (Centrum Nauki Kopernik)",
         "address": "Wybrzeże Kościuszkowskie 20, Warsaw, Poland",
         "cost": "Adults: 25 PLN, Students: 19 PLN",
         "description": "Interactive science museum with hands-on exhibits, planetarium, designed to engage visitors of all ages in science, nature, and technology.",
-        "photo_url": "images/copernicus-science-centre.webp"
+        "photo": "images/copernicus-science-centre.webp"
     },
     {
-        "name": "Warsaw Uprising Museum (Muzeum Powstania Warszawskiego)",
+        "title": "Warsaw Uprising Museum (Muzeum Powstania Warszawskiego)",
         "address": "ul. Grzybowska 79, 00-844 Warsaw, Poland",
         "cost": "Adults: 25 PLN, Students: 15 PLN",
         "description": "Dedicated museum chronicling the 1944 Warsaw Uprising. Exhibits include artifacts, films, interactive displays, reconstructed streets, and immersive experiences.",
-        "photo_url": "images/uprising-museum.webp"
+        "photo": "images/uprising-museum.webp"
     },
     {
-        "name": "National Museum in Warsaw (Muzeum Narodowe w Warszawie)",
+        "title": "National Museum in Warsaw (Muzeum Narodowe w Warszawie)",
         "address": "Al. Jerozolimskie 3, Warsaw, Poland",
         "cost": "Adults: 25 PLN, Students: 15 PLN",
         "description": "One of Poland’s oldest museums featuring art collections from antiquity to modern art, including Polish paintings, sculptures, relics, and decorative arts.",
-        "photo_url": "images/national-museum.webp"
+        "photo": "images/national-museum.webp"
     },
     {
-        "name": "University of Warsaw Botanical Garden",
+        "title": "University of Warsaw Botanical Garden",
         "address": "Aleje Ujazdowskie 4, Warsaw, Poland",
         "cost": "Adults: 10 PLN, Students: free or reduced on certain days",
         "description": "Relaxing green space in the city featuring plant collections, ponds, shade trees and paths. Good for walking, reading, enjoying nature in an urban setting.",
-        "photo_url": "images/university-garden.webp"
+        "photo": "images/university-garden.webp"
     }
 ]
--- chamber/scripts/place-cards.js
+++ chamber/scripts/place-cards.js
@@ -1,2 +1,49 @@
-import { places } from "../data/places.mjs"
+import { places } from "../data/places.mjs";
 console.log(places);
+
+createCardDisplay(places);
+
+function createCardDisplay(places) {
+    places.forEach(place => {
+        // Set Variables
+        let card = document.createElement("div"); // Card
+
+        let photo = document.createElement("figure"); // Photo
+
+        let info = document.createElement("div")
+        let title = document.createElement("h2"); // Title
+        let address = document.createElement("address"); // Address
+        let description = document.createElement("p"); // Description
+        let button = document.createElement("button"); // Button
+
+
+        // Set Attributes
+        card.setAttribute("class", "card"); // Card
+
+        photo.setAttribute("class", "photo"); // Photo
+
+        info.setAttribute("class", "info");
+        title.setAttribute("class", "title"); // Title
+        address.setAttribute("class", "address"); // Address
+        description.setAttribute("class", "description"); // Description
+        button.setAttribute("class", "learn-more"); // Button
+
+        // Set Card Info
+        photo.innerHTML = `<img src="${place.photo}" alt="photo" loading="lazy">`;
+
+        title.textContent = `${place.title}`;
+        address.textContent = `${place.address}`;
+        description.textContent = `${place.description}`;
+        button.textContent = `Learn More`;
+
+        // Add Display to HTML
+        card.appendChild(photo);
+        card.appendChild(info);
+        info.appendChild(title);
+        info.appendChild(address);
+        info.appendChild(description);
+        card.appendChild(button);
+
+        document.querySelector("#place-cards").appendChild(card);
+    });
+}
